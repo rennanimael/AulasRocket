1006b9bd085090926d8d4fd88148420fe9f6c4a9
Imael Rennan <imael.rennan@ufms.br>
2022-03-18 19:34:39 -0400

    primeiro script de Object

---
+++ ObjectInitial.js
@@ -0,0 +1,16 @@
+
+/*objetos possui propriedades e funcionalidades
+suas propriedades podem ser ditas como características do objeto, por exemplo uma pessoa tem nome, idade, cpf, cor do cabelo, etc.
+suas funcionalidades são ações que podem ser realizadas por essa pessoa, como programar, andar, limpar a casa entre outros */
+console.log(
+    {
+    name: "rennan", //definindo propriedade
+    idade: 22, //definindo propriedade
+    programar: function(){ //definindo uma funcionalidade
+        console.log('programar');
+    }
+    }
+    )
+
+
+
