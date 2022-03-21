216bbc53b340a35bcd4a24ec8a0890218cd5d50e
Imael Rennan <imael.rennan@ufms.br>
2022-03-21 09:17:00 -0400

    Revisão de declaração de variáveis e atribuição de valores

---
+++ declarationAndassignment.js
@@ -0,0 +1,14 @@
+let name //declaration(declaração de variável)
+
+name = 'Rennan' //assignment(atribuir um valor a essa variável)
+
+console.log(typeof name) //typeof antes da variável para saber o tipo de dado
+console.log(name) // para imprimir o valor armazenado na variável name
+
+let age, isOnline //Agrupamento de variáveis(declarações simultâneas)
+
+age = 22
+isOnline = true
+
+console.log(name,age, true) //Para imprimir multiplas variáveis em um único console.log devemos separálos com virgulas
+
