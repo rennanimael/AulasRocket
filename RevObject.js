95a0d968a8d064f00c7d83333a6b2783fafafb6f
Imael Rennan <imael.rennan@ufms.br>
2022-03-21 09:54:31 -0400

    Revisão básica Object

---
+++ RevObject.js
@@ -0,0 +1,16 @@
+const person = {
+    name: 'Rennan',
+    age: 22,
+    weight: '70.2 KG',
+    isBrazilian: true
+
+}
+//print the object
+console.log(person)
+//imprimindo uma propriedade do object
+console.log(person.name) // utilizamos o nomedoObject.propriedade 
+//Imprimindo com iterpolação
+console.log(`${person.name} tem ${person.age} anos e pesa ${person.weight}`)
+
+
+//usamos virgula para separar cada valor de propriedade no Object
