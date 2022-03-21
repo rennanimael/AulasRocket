9ee964e1b68a259eb50d8d4c6de9a2753fd87734
Imael Rennan <imael.rennan@ufms.br>
2022-03-21 09:37:58 -0400

    Concatenando e Interpolando no console.log comentario atualizado

--- concatenandoeinterpolando.js
+++ concatenandoeinterpolando.js
@@ -4,4 +4,4 @@ age = 23
 
 console.log('o ' + name + ' tem ' + age + ' anos.') //formatando texto com variável utilizando concatenação, texto dentro de aspas e com sinal de soma juntamos o texto com a variável
 
-console.log(`O ${name} tem ${age} anos.`) //formatando texto com variável utilizando a interpolação, utiliza-se crase, variáveis são embrulhadas com cifrão e chaves
+console.log(`O ${name} tem ${age} anos.`) //formatando texto com variável utilizando a interpolação, utiliza-se crase, variáveis são embrulhadas com cifrão e chaves(template literal or template strings)
